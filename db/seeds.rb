b25cfa5bdefe774b159307c7e7ae2434983fb014
masa-self-learning <selflearning4it@gmail.com>
2021-08-07 21:07:36 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,20 @@ Admin.create!(name: name,
              password: password,
              password_confirmation: password)
 
+name = "デモ社員"
+email = "demo@example.com"
+user_no = "1111"
+password = "123456"
+User.create!(name: name,
+             email: email,
+             user_no: user_no,
+             password: password,
+             password_confirmation: password)
+
  9.times do |n|
-  name = "太郎#{n+1}"
-  email =  "taro#{n+1}@example.com"
-  user_no = "111#{n}"
+  name = "太郎#{n}"
+  email =  "taro#{n}@example.com"
+  user_no = "222#{n}"
   password = "123456"
   User.create!(name: name,
                email: email,
@@ -20,9 +30,9 @@ Admin.create!(name: name,
                password_confirmation: password)
 end
 
-30.times do |n|
+61.times do |n|
   from = Date.parse("2021/06/01")
-  to = Date.parse("2021/06/30")
+  to = Date.parse("2021/07/31")
   term = Array(from..to)
   date = term[n]
   start_time = "09:00"
